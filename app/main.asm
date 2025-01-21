66df5fdc44ac6dd2c32c3f94fdf0680ac754916a
Peter Buckley <peterabbit22.22@gmail.com>
2025-01-20 23:05:11 -0700

    Created flow chart and validated timings

--- app/main.asm
+++ app/main.asm
@@ -21,7 +21,7 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
 
 ;-------------------------------------------------------------------------------
-; Main loop here
+; Main loop here (Timer or Delay can be move dto the top to pick which method it uses)
 ;-------------------------------------------------------------------------------
 SetupP1:     
         bic.b   #BIT0,&P1OUT            ; Clear P1.0 output
@@ -29,21 +29,30 @@ SetupP1:
         bic.w   #LOCKLPM5,&PM5CTL0      ; Unlock I/O pins
 
 Mainloop:
-        jmp     Timer                   ; Jump to timer subroutine
         jmp     Delay                   ; Jump to delay subroutine
+        jmp     Timer                   ; Jump to timer subroutine
+
 
 ;-------------------------------------------------------------------------------
 ; Delay Loop
 ;-------------------------------------------------------------------------------
-Delay:    
-        xor.b   #BIT0,&P1OUT            ; Toggle P1.0 every 0.5s
-Wait:      
-        mov.w   #25000,R15             ; Delay to R15
-L1:         
-        dec.w   R15                     ; Decrement R15
-        jnz     L1                      ; Delay over?
-        jmp     Delay                   ; Again
-        NOP
+Delay:
+        xor.b   #BIT0, &P1OUT          ; Toggle P1.0 every 0.5s
+        mov.w   #2, R14                ; Outer loop count
+OuterLoop:
+        mov.w   #65000, R15            ; Inner loop count (0.5 second period maxes out 16bit register)
+InnerLoop:
+        dec.w   R15                    ; Decrement R15
+        jnz     InnerLoop              ; Inner loop done?
+        dec.w   R14                    ; Decrement R14
+        jnz     OuterLoop              ; Outer loop done?
+
+        mov.w   #44000, R15            ; Remaining cycles
+InnerLoop2:
+        dec.w   R15                    ; Decrement R15
+        jnz     InnerLoop2             ; Finish remaining cycles
+
+        jmp     Delay                  ; Repeat the loop
 
 ;-------------------------------------------------------------------------------
 ; Timer Setup and loop
@@ -55,7 +64,7 @@ Timer:
 		bis.w	#MC__UP, &TB0CTL
 
 		; Setup Compare Register
-		mov.w	#250000, &TB0CCR0
+		mov.w	#16384, &TB0CCR0
 
 		bis.w	#CCIE, &TB0CCTL0
 		bic.w	#CCIFG, &TB0CCTL0
